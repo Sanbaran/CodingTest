202465d5db3c14ba8b276667969512bd4f9cbdca
Sanbaran Sengupta <sanbaran.rns@gmail.com>
2020-08-31 22:36:22 +0530

    Basic Structure

---
+++ ManageCart/PromotionEngine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ManageCart
+{
+    public class PromotionEngine
+    {
+    }
+}
---
+++ TestPromotionEngine/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPromotionEngine
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Scenario1Test()
+        {
+        }
+    }
+}
