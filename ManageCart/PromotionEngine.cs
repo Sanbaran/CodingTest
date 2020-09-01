6cc3701541320c0a09e93ddd871c4708de1716e8
Sanbaran Sengupta <sanbaran.rns@gmail.com>
2020-09-01 13:02:34 +0530

    Scenario A passes

--- ManageCart/PromotionEngine.cs
+++ ManageCart/PromotionEngine.cs
@@ -1,8 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ManageCart
 {
     public class PromotionEngine
     {
+        public decimal CalculateTotal(IEnumerable<CartSKU> productsPurchased, IEnumerable<Promotion> activePromotions)
+        {
+            if (productsPurchased.Count() == 0)
+            {
+                return 0;
+            }
+
+            if(activePromotions.Count() == 0)
+            {
+                return productsPurchased.Select(x => x.UnitPrice * x.UnitsPurchased).Sum();
+            }
+
+            decimal total = 0;
+            // apply the promotions
+            foreach (Promotion promotion in activePromotions)
+            {
+                var elligibleSKUs = productsPurchased.Intersect(promotion.SKUs, new SKUComparer());
+                if (elligibleSKUs.Count() == promotion.SKUs.Count())
+                {
+                    // check if the promotion has a fixed price
+                    if(promotion.FixedPrice.HasValue)
+                    {
+                        total += promotion.FixedPrice.Value;
+                    }
+                    else
+                    {
+                        // else calculate based on the % discount on unit price
+                    }
+
+                    //remove the SKUs on which promotion has been applied
+                    foreach(var sku in promotion.SKUs)
+                    {
+                        productsPurchased.Where(x => string.Equals(x.Id, sku.Id)).FirstOrDefault().UnitsPurchased -= sku.UnitsPurchased;
+                    }
+                }
+            }
+
+            // calulate the SKUs left out after applying promotions
+            foreach(var sku in productsPurchased)
+            {
+                total += sku.UnitPrice * sku.UnitsPurchased;
+            }
+
+            return total;
+        }
+    }
+
+    public class SKU
+    {
+        public string Id { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+
+    public class CartSKU : SKU
+    {
+        public int UnitsPurchased { get; set; }
+    }
+
+    public class PromotionSKU : CartSKU
+    {
+        public decimal? DiscountOnUnitPrice { get; set; }
+    }
+
+    public class Promotion
+    {
+        public Promotion()
+        {
+            this.SKUs = new List<PromotionSKU>();
+        }
+
+        public IEnumerable<PromotionSKU> SKUs { get; set; }
+        public decimal? FixedPrice { get; set; }
+    }
+
+    public class SKUComparer : IEqualityComparer<CartSKU>
+    {
+        public int GetHashCode(CartSKU a)
+        {
+            if (a == null)
+            {
+                return 0;
+            }
+
+            return a.Id.GetHashCode();
+        }
+
+        public bool Equals(CartSKU a, CartSKU b)
+        {
+            if (string.Equals(a.Id, b.Id) && a.UnitsPurchased <= b.UnitsPurchased)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- TestPromotionEngine/UnitTest1.cs
+++ TestPromotionEngine/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ManageCart;
 
 namespace TestPromotionEngine
 {
@@ -6,8 +8,81 @@ namespace TestPromotionEngine
     public class UnitTest1
     {
         [TestMethod]
-        public void Scenario1Test()
+        public void ScenarioATest()
         {
+            //define test input and output values
+            decimal expectedResult = 100;
+            IEnumerable<CartSKU> purchasedSKUs = GetPurchasedSKUs();
+            IEnumerable<Promotion> activePromotions = GetActivePromotions();
+                        
+            PromotionEngine systemUnderTest = new PromotionEngine();
+            decimal actualResult = systemUnderTest.CalculateTotal(purchasedSKUs, activePromotions);
+
+            //verify the result
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        private static IEnumerable<Promotion> GetActivePromotions()
+        {
+            return new List<Promotion>()
+            {
+                new Promotion()
+                {
+                    SKUs = new List<PromotionSKU>()
+                    {
+                        new PromotionSKU()
+                        {
+                            Id = "A", UnitPrice = 50, UnitsPurchased = 3
+                        }
+                    },
+                    FixedPrice = 130
+                },
+                new Promotion()
+                {
+                    SKUs = new List<PromotionSKU>()
+                    {
+                        new PromotionSKU()
+                        {
+                            Id = "B", UnitPrice = 30, UnitsPurchased = 2
+                        }
+                    },
+                    FixedPrice = 45
+                },
+                new Promotion()
+                {
+                    SKUs = new List<PromotionSKU>()
+                    {
+                        new PromotionSKU()
+                        {
+                            Id = "C", UnitPrice = 20, UnitsPurchased = 1
+                        },
+                        new PromotionSKU()
+                        {
+                            Id = "D", UnitPrice = 15, UnitsPurchased = 1
+                        }
+                    },
+                    FixedPrice = 30
+                },
+            };
+        }
+
+        private IEnumerable<CartSKU> GetPurchasedSKUs()
+        {
+            return new List<CartSKU>()
+            {
+                new CartSKU()
+                {
+                    Id = "A", UnitPrice = 50, UnitsPurchased = 1
+                },
+                new CartSKU()
+                {
+                    Id = "B", UnitPrice = 30, UnitsPurchased = 1
+                },
+                new CartSKU()
+                {
+                    Id = "C", UnitPrice = 20, UnitsPurchased = 1
+                }
+            };
         }
     }
 }
